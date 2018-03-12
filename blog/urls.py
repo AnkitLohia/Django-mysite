9c40864b50da018542951ddf14e766259a9e4f4f
Ankit Lohia <ankitlohia1992@gmail.com>
2018-03-12 16:40:32 +0530

    Showing blog posts sequentially

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
                 url(r'^$', ListView.as_view(
                                     queryset=Post.objects.all().order_by("-date")[:25], #list of 25 blogpost order by date desc.
                                     template_name="blog/blog.html")),
+                url(r'^(?P<pk>\d+)$', DetailView.as_view(
+                                    model = Post,
+                                    template_name="blog/post.html")),
             ]
